dde63d8b93dd8c00e8558272c85194874ad50768
drdonley53 <daviddonley@Davids-MacBook-Pro.local>
2015-01-17 22:25:22 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function yes(){
+	document.getElementById("answer").innerHTML = "Please go get your Reward";
+	
+}
+
+function no(){
+	document.getElementById("answer").innerHTML = "Please keep an eye out";
+	
+}
