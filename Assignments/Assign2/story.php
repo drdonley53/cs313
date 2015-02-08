a8f2859452c24dad75012f311f8c5f22c33ae8af
drdonley53 <daviddonley@Davids-MacBook-Pro-2.local>
2015-02-07 22:34:59 -0700

    update

--- Assignments/Assign2/story.php
+++ Assignments/Assign2/story.php
@@ -14,6 +14,44 @@
 	<button id="btn" onclick="location.href='genre.php'">genre</button></div>
 <?php
 
+function loadDatabase()
+{
+
+  $dbHost = "";
+  $dbPort = "";
+  $dbUser = "";
+  $dbPassword = "";
+
+     $dbName = "testdb";
+
+     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+     if ($openShiftVar === null || $openShiftVar == "")
+     {
+          // Not in the openshift environment
+          //echo "Using local credentials: "; 
+          require("setLocalDatabaseCredentials.php");
+     }
+     else 
+     { 
+          // In the openshift environment
+          //echo "Using openshift credentials: ";
+
+          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     } 
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+
+     $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+     return $db;
+
+}
+
+
+
 	$db = new PDO("mysql:host=localhost;dbname=author_web", 'Ddonley', 'passW');
 
 	try
